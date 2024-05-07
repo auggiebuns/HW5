522e67f3d020b0d156c26b068992f875e24f2dd4
auggiebuns <augustad17@uchicago.edu>
2024-05-06 22:54:14 -0500

    changed except

--- pa5.py
+++ pa5.py
@@ -55,7 +55,9 @@ def bisection_root(func, variable1, variable2):
             else:
                 variable1 = x_mid
                 y_pred1 = y_mid
-    except:
-            raise ValueError("Failed to find a root within the tolerance")
+        raise ValueError("Failed to find a root within the tolerance")    
+    except Exception as exc:
+        print("Warning:", exc)
+        raise ValueError("Failed to find a root within the tolerance") from exc
 
 ROOT = bisection_root(math.sin, 2,4)
